d6bb6716daccd4d4baf76b471cd314d62a92ddef
pragya <pragyakhare0626@gmail.com>
2022-01-18 18:13:51 +0530

    added msg

---
+++ diff.java
@@ -0,0 +1,7 @@
+public class diff {
+    public static void main(String[] args) {
+        int a=2,b=3;
+        int sum=a-b;
+        System.out.println(sum);
+    }
+}
--- sum.java
+++ sum.java
@@ -2,6 +2,6 @@ class sum{
     public static void main(String[] args) {
         int a=2,b=3;
         int sum=a+b;
-        System.out.println(sum);
+        System.out.println("sum of a and b"+sum);
     }
 }

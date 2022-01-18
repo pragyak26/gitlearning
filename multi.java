6be4482f3dc882e1f4046eebe12a6a00884c91d1
pragya <pragyakhare0626@gmail.com>
2022-01-18 18:26:28 +0530

    added multi.java

---
+++ multi.java
@@ -0,0 +1,7 @@
+public class multi {
+    public static void main(String[] args) {
+        int a=2,b=3;
+        int sum=a*b;
+        System.out.println("multiplication of a and b"+sum);
+    }
+}
